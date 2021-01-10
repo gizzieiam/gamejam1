f0dd2eca68c46a139df3047bc4317e518d6024a4
codenameGizmo <shavonnecarson94@gmail.com>
2021-01-09 18:14:36 -0800

    npc spawner

--- NpcSpawner.cs
+++ NpcSpawner.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class NpcSpawner : MonoBehaviour
 {
+    public Transform[] spawnPoints;
+    public GameObject[] npcs;
+    public static bool spawnAllowed;
+    int randomSpawnPoint, randomNpc;
+    public int count = 5;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        spawnAllowed = true;
+        InvokeRepeating("SpawnANpc", 0f, 1f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(count == 0)
+        {
+            spawnAllowed = false;
+        }
+    }
+    void SpawnANpc()
+    {
+        if(spawnAllowed)
+        {
+            randomSpawnPoint = Random.Range(0, spawnPoints.Length);
+            randomNpc = Random.Range(0, npcs.Length);
+            Instantiate(npcs[randomNpc], spawnPoints[randomSpawnPoint].position,
+                Quaternion.identity);
+                count = count - 1;
+        }
     }
 }
--- readme.md
+++ readme.md
@@ -1,3 +1,7 @@
 # My First Game Jam
 ## Topic: The World
 ## Time Frame: 2 weeks
+
+## Game Concept
+
+- open world quest game
